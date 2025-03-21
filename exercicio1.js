72507f3787efb408a61da7d56c44b3b3fb29c265
Noraami <gabriela_pimentel@estudante.sesisenai.org.br>
2025-03-21 10:11:33 -0300

    Exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,22 @@
+function somarNumeros() {
+    let soma = 0;
+    let continuar = true;
+
+    while (continuar) {
+        let numero = parseInt(prompt("Digite um número (ou digite 0 para encerrar):"));
+
+        if (isNaN(numero)){
+            alert("Digitar um número válido");
+        } else {
+            soma += numero;
+        }
+
+        if (numero == 0){
+            continuar = false;
+        }
+    }
+
+    alert("A soma dos números é: " + soma);
+}
+
+
