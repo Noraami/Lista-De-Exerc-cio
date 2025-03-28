9d9ebd70768136c208ba3d4fdb3e3a53f1c6f4d9
Noraami <gabriela_pimentel@estudante.sesisenai.org.br>
2025-03-28 08:30:04 -0300

    2

---
+++ exercicio2.js
@@ -0,0 +1,20 @@
+function contagem(duration, display){
+    var timer = duration, minutes, seconds;
+
+    setInterval(function(){
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ":" + seconds;
+        if(--timer < 0){
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function(){
+    var duration = 60 * 4;
+    var display = document.querySelector("#timer");
+    contagem(duration, display);
+}
