c71402a1c46b12c05cc9559254c73aad1c221b94
Noraami <gabriela_pimentel@estudante.sesisenai.org.br>
2025-03-31 12:23:56 -0300

    6

---
+++ exercicio6.js
@@ -0,0 +1,12 @@
+function vog(){
+    letra = prompt("Insira uma letra:");
+
+    if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u" || letra == "A" || letra == "E" || letra == "I" || letra == "O" || letra == "U"){
+
+        alert("A letra - " + letra + " - é uma vogal");
+
+    } else {
+        alert("A letra - " + letra + " - é uma consoante");
+
+    }
+}
